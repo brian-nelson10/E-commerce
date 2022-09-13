3dd4d89f3371131ad88f94b2d6dccf048925d472
brian nelson <bn3l10@gmail.com>
2022-09-13 09:42:58 -0400

    added models

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,28 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    category_name: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    products: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'product',
+        key: 'id',
+        
+      }
+    }
   },
   {
     sequelize,
